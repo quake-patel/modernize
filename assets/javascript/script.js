4ed6220415876578ea68fbf370c7abab513abceb
quake-patel <quake.patel@gmail.com>
2024-01-05 20:11:13 +0530

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $(".search-btn").on("click", function (event) {
+    $(".group").toggleClass("none");
+    event.stopPropagation();
+  });
+
+  $(".menu-toggler").click(() => {
+    $("body").toggleClass("sidebar-closed sidebar-expanded");
+  });
+  $(".close-mobile-sidebar").click(() => {
+    $("body").removeClass("sidebar-expanded");
+  });
+
+  $(document).on("click", function (event) {
+    if (!$(event.target).closest(".group").length) {
+      $(".group").addClass("none");
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(".accordian-btn").on("click", function () {
+    $(this).next(".accordian").slideToggle();
+    $(this).$("span svg").toggleClass("rotate");
+  });
+});
