db2fdc23baaac687fe0039a25881e157309def4d
quake-patel <quake.patel@gmail.com>
2024-01-08 22:51:38 +0530

    css big

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,24 @@
 $(document).ready(function () {
-  $(".search-btn").on("click", function (event) {
-    $(".group").toggleClass("none");
-    event.stopPropagation();
-  });
-
+  // menu
   $(".menu-toggler").click(() => {
     $("body").toggleClass("sidebar-closed sidebar-expanded");
   });
   $(".close-mobile-sidebar").click(() => {
     $("body").removeClass("sidebar-expanded");
   });
-
-  $(document).on("click", function (event) {
+  // search
+  $(".search-btn").on("click", function (event) {
+    $(".group").toggleClass("none");
+    event.stopPropagation();
+  });
+  $(document).click((event) => {
     if (!$(event.target).closest(".group").length) {
       $(".group").addClass("none");
     }
   });
 });
-
-$(document).ready(function () {
-  $(".accordian-btn").on("click", function () {
-    $(this).next(".accordian").slideToggle();
-    $(this).find("svg").toggleClass("rotate");
-  });
+// accordian
+$(".accordian-btn").on("click", function () {
+  $(this).next(".accordian").slideToggle();
+  $(this).find("span svg").toggleClass("rotate");
 });
